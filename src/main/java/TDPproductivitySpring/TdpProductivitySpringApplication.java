610e00eacdbdce08ca9a71bb8ffbc74acca16171
REP <inge@van-haaften.nl>
2020-04-21 15:38:08 +0200

    Default setup

---
+++ src/main/java/TDPproductivitySpring/TdpProductivitySpringApplication.java
@@ -0,0 +1,13 @@
+package TDPproductivitySpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdpProductivitySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdpProductivitySpringApplication.class, args);
+	}
+
+}
