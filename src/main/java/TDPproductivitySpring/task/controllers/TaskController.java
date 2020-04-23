b9399936ebb1e296abaa5c1220bd0273ccc23b5f
EdwinBolt <edwinbolt@live.nl>
2020-04-23 14:39:38 +0200

    time

--- src/main/java/TDPproductivitySpring/task/controllers/TaskController.java
+++ src/main/java/TDPproductivitySpring/task/controllers/TaskController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,7 +27,35 @@ public class TaskController {
     @GetMapping(url)
     public List<Task> findAll(){
         Iterable<Task> temp = taskService.findAll();
-        System.out.println(temp);
+        //System.out.println(temp);
+
+        List<LocalTime> projectTimes = new ArrayList<>();
+
+
+//
+        for (Task task : temp){
+            if (projectTimes.size()>=task.project.getId()){
+                LocalTime currentTime, newTime;
+
+                currentTime = projectTimes.get(task.project.getId());
+                newTime = currentTime.plusMinutes(task.getDuration().getMinute()).plusHours(task.getDuration().getHour());
+
+                projectTimes.set(task.project.getId()-1,newTime);
+
+            }
+            else{
+                projectTimes.add(task.getDuration());
+            }
+//            System.out.println(task.getClass());
+//            System.out.println("----");
+//            System.out.println(task.project.getId());
+        }
+
+        for (LocalTime time:projectTimes){
+            System.out.println(time);
+        }
+
+
         return (List<Task>)taskService.findAll();
 
     }
@@ -39,7 +69,7 @@ public class TaskController {
     @PutMapping(url)
     public Task update(@RequestBody Task task){
         return taskService.save(task);
-        }
+    }
 
     @ResponseStatus(value = HttpStatus.OK)
     @DeleteMapping(url +"/{id}")
