98b880afda2df956cba4e70f88707053088d6e25
EdwinBolt <edwinbolt@live.nl>
2020-05-12 15:33:17 +0200

    final

--- src/main/java/TDPproductivitySpring/task/controllers/TaskController.java
+++ src/main/java/TDPproductivitySpring/task/controllers/TaskController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TaskController {
     final String url="/task";
